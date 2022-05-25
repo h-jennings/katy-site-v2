6ce53595fad7fc3b7865ad11cd171e25807a8145
Hunter Jennings <jenningsdhunter@gmail.com>
2022-05-24 20:28:09 -0400

    Chore: configuring bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'default',
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([withBundleAnalyzer], nextConfig);
