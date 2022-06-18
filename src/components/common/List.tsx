2a7d3a111a6b972df4f447a57ce987f8bb71daef
Hunter Jennings <jenningsdhunter@gmail.com>
2022-06-18 09:51:20 -0400

    feat(rich-text): Adding RichText component
    - closes: #11

--- src/components/common/List.tsx
+++ src/components/common/List.tsx
@@ -1,5 +1,17 @@
 import { styled } from '@/styles/stitches.config';
 
+export const StyledList = styled('ul', {
+  listStyleType: 'disc',
+  margin: 0,
+  marginBottom: '$4',
+  padding: 0,
+  paddingLeft: '$5',
+  'ul,ol': {
+    marginTop: '$3',
+    marginBottom: '$3',
+  },
+});
+
 export const UnstyledList = styled('ul', {
   listStyleType: 'none',
   margin: 0,
