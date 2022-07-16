e587bf8e210ed3909dbfa55c732cc0e286542916
Hunter Jennings <jenningsdhunter@gmail.com>
2022-07-16 10:58:22 -0400

    cypress and eslint love each other now

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    supportFile: false,
     baseUrl: 'http://localhost:3000',
     video: false,
     viewportWidth: 1280,
