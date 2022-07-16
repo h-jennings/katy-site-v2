a2229f2dab594399a9fed088533d9a43fa8f5a89
Hunter Jennings <jenningsdhunter@gmail.com>
2022-07-16 11:36:48 -0400

    Not screenshot on failure

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     supportFile: false,
+    screenshotOnRunFailure: false,
     baseUrl: 'http://localhost:3000',
     video: false,
     viewportWidth: 1280,
